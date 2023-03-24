b5d785a21319a912dc56a8cee90bbb6b41205cfd
jojoner16 <laybats.jonathan@orange.fr>
2023-03-24 11:25:33 +0100

    Résolution [Bug Report] - Login, ajout de commentaires sur les modifications

--- Billed-app-FR-Front/src/containers/Login.js
+++ Billed-app-FR-Front/src/containers/Login.js
@@ -46,6 +46,29 @@ export default class Login {
       });
   };
 
+  // [Bug report] - Login test employee au lieu de admin
+  // handleSubmitAdmin = (e) => {
+  //   e.preventDefault();
+  //   const user = {
+  //     type: "Admin",
+  //     email: e.target.querySelector(`input[data-testid="employee-email-input"]`)
+  //       .value,
+  //     password: e.target.querySelector(
+  //       `input[data-testid="employee-password-input"]`
+  //     ).value,
+  //     status: "connected",
+  //   };
+  //   this.localStorage.setItem("user", JSON.stringify(user));
+  //   this.login(user)
+  //     .catch((err) => this.createUser(user))
+  //     .then(() => {
+  //       this.onNavigate(ROUTES_PATH["Dashboard"]);
+  //       this.PREVIOUS_LOCATION = ROUTES_PATH["Dashboard"];
+  //       PREVIOUS_LOCATION = this.PREVIOUS_LOCATION;
+  //       document.body.style.backgroundColor = "#fff";
+  //     });
+  // };
+
   //Résolution [Bug report] - Login
   handleSubmitAdmin = (e) => {
     e.preventDefault();
